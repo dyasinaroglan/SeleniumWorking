710e18499e4242ed9ed789050ff4f1cc2f585d5d
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-02-12 14:26:47 +0300

    12.02.22

---
+++ src/Class1.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\SELENIUM\\chromeDriver\\chromedriver.exe");
+                                   //key--propertyName                         value
+        //"C:\SELENIUM\chromeDriver\chromedriver.exe" CTRL+SHIFT+MOUSE SAĞ TIKLAYIP---yol olarak kopyala dediğimzde geliyor.
+        /*
+        setProperty--->
+        amacı hangi tarayıcın test edileceğine karar vermek ve web uygulaması için
+        testleri otomatikleştirmenin ilk adımıdır. sistem özelliklerini tanımlamak
+        ve tarayıcı nesnesini başlatmak gerekli hale gelir.
+         */
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");  //test edeceğimiz driveri getirdik.
+        driver.manage().window().maximize();  //driveri yönet ve pencereye maximize et, büyüt.
+        driver.close();
+
+        /*
+        1.id --- ile element bulma (kimlik)
+        2.name---- ile element bulma (ad) (soyad)
+        3.className --- ile element bulma (sınıf adı)
+        4.LinkText    (bağlantı linki)
+        5.partialLinkText kısmi bağlantı linki)
+        6.tagName  (etiket adı)
+        7.cssSelector  todo tagAdı[@özellikAdı ='deger']
+        8.xpath
+         */
+        Thread.sleep(2000);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user"); //element bul
+        //hangi özelliği ile bulucam By.id--kimliği ile bul
+        Thread.sleep(3000);
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        //sendKeys--anahtar kelime gönder diyor bana
+        driver.findElement(By.id("login-button")).click(); //click tıklayın(tıklamamız lazım)
+        Thread.sleep(3000);
+        driver.close();
+        //
+
+
+    }
+}
