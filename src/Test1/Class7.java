f04c20ffdfdd1bc2c6030463a0cc424d45311229
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-02-14 16:41:01 +0300

    14.02.22

---
+++ src/Test1/Class7.java
@@ -0,0 +1,43 @@
+package Test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class7 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\SELENIUM\\chromeDriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.phptravels.net/login");
+        driver.manage().window().maximize();
+
+        WebElement email = driver.findElement(By.xpath("//input[@class='form-control']"));
+        email.sendKeys("yasin aroglan");
+
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password'] "));
+        password.sendKeys("Yasin.264264");
+
+        WebElement rememberReset = driver.findElement(By.xpath("//label[@for='rememberchb']"));
+        if(rememberReset.isEnabled()){
+            try {
+                rememberReset.click();
+            }catch (Exception e){
+                rememberReset.click();
+            }
+        }
+        boolean isRememberReset = rememberReset.isEnabled();
+        System.out.println(isRememberReset);
+
+        WebElement logın = driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg btn-block effect ladda-button waves-effect']"));
+        boolean isLogın = logın.isEnabled();
+        System.out.println(isLogın);
+
+        WebElement phone = driver.findElement(By.xpath("//div/i[@class='la la-phone']"));
+        phone.isEnabled();
+
+
+
+    }
+}
