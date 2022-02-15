5af548a9bd45d7b87a577ddd3fef10e010aa300a
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-02-15 12:55:33 +0300

    15.02.22

---
+++ src/DemogaTest/Class6_Demoga.java
@@ -0,0 +1,33 @@
+package DemogaTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Class6_Demoga {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\SELENIUM\\chromeDriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/buttons");
+
+        WebElement doubleClickMe = driver.findElement(By.id("doubleClickBtn"));
+        Actions actions = new Actions(driver); //Actions class ı WebDriver sınıfından bir değişken almalı.
+        actions.doubleClick(doubleClickMe).perform();
+
+        WebElement message = driver.findElement(By.id("doubleClickMessage"));
+        String textMessage =message.getText();
+        System.out.println(textMessage);
+
+        WebElement rightClickMe = driver.findElement(By.id("rightClickBtn"));
+        actions.contextClick(rightClickMe).perform();
+                //contextClick() iki tane methodu var parametreli ve parametresiz.
+                //biz WebDriver yazan parametreli olanı seçiyoruz. ve içine WebDriver değişkenini yani rightClickMe yi ekliyoruz.
+
+        WebElement rightClickMessage = driver.findElement(By.id("rightClickMessage"));
+        String message2 = rightClickMessage.getText();
+        System.out.println(message2);
+    }
+}
